9c0f683598f28e50eb70648b47b96bdab014f1ec
Wayne Phillips <wayne@phillips1.com>
2022-03-07 17:42:28 -0600

    test updates

--- eth-contracts/test/TestSolnSquareVerifier.js
+++ eth-contracts/test/TestSolnSquareVerifier.js
@@ -6,18 +6,19 @@ var proof = require('../../zokrates/code/square/proof');
 contract('TestSolnSquareVerifier', async(accounts) => {
     beforeEach(async function () {
         let verifier = await Verifier.new({ from: accounts[0] });
-        this.contract = await SolnSquareVerifier.new(verifier.address, { from: accounts[0] });
+        this.contract = await SolnSquareVerifier.new(verifier.address, "WaynesRealEstateToken", "WSPPROPNFT", { from: accounts[0] });
     });
 
     // Test if a new solution can be added for contract - SolnSquareVerifier
     it('Test if a new solution can be added for contract - SolnSquareVerifier', async function () {
-        await this.contract.addSolution(101,accounts[0]);
-        let numSolutions = await this.contract.totalSolution.call();
-        assert.equal(numSolutions.toNumber(), 1, "New Solution was not added.");
+        // using example shown here to capture eventEmitted to prove the solution was added... https://github.com/rkalis/truffle-assertions
+        let tx = await this.contract.AddSolution(101,accounts[1],{ from: accounts[0] });
+        truffleAssert.eventEmitted(tx,"SolutionAdded");
     })
 
     // Test if an ERC721 token can be minted for contract - SolnSquareVerifier
     it('Test if an ERC721 token can be minted for contract - SolnSquareVerifier', async function () {
-      await truffleAssert.reverts(await this.contract.Mint_WaynesRealEstateToken(914, accounts[0], proof.proof.a, proof.proof.b, proof.proof.c, proof.inputs, {from: account_one}),null,"No Error Was thrown trying to mint a token when address is not contract owner");
+        let tx = await this.contract.Mint_WaynesRealEstateToken(914, accounts[1], proof.proof, proof.inputs, {from: accounts[0]});
+        assert.equal(await this.contract.ownerOf(914), accounts[1], "Cannot verify that the token was minted for the specified owner.");
     })
   })
